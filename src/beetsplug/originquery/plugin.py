33f0117382e0d236194f42e514c779bddda09bcb
Peter Dolan <peter@peterdolan.us>
2026-02-15 09:37:05 -0500

    Add support for 'barcode' field

--- src/beetsplug/originquery/plugin.py
+++ src/beetsplug/originquery/plugin.py
@@ -18,13 +18,14 @@ BEETS_TO_LABEL = OrderedDict(
         ("year", "Edition year"),
         ("country", "Country"),
         ("label", "Record label"),
+        ("barcode", "Barcode"),
         ("catalognum", "Catalog number"),
         ("albumdisambig", "Edition"),
     ]
 )
 
 # Conflicts will be reported if any of these fields don't match.
-CONFLICT_FIELDS = ["catalognum", "media"]
+CONFLICT_FIELDS = ["barcode", "catalognum", "media"]
 
 
 def escape_braces(string):
